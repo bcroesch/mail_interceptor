b84ed6ca9923dbdfca918270efbbb3608cc41893
Ben Roesch <ben@flyoverworks.com>
2015-11-04 11:25:20 -0600

    Don't perform delivery at all if domain isn't in deliver to domains

--- lib/mail_interceptor.rb
+++ lib/mail_interceptor.rb
@@ -8,16 +8,15 @@ module MailInterceptor
 
     def initialize options = {}
       @deliver_emails_to = Array.wrap options[:deliver_emails_to]
-      @forward_emails_to = options.fetch :forward_emails_to
       @env               = options.fetch :env, InterceptorEnv.new
-
-      sanitize_forward_emails_to
     end
 
     def delivering_email message
       message.to = normalize_recipients(message.to).flatten.uniq unless message.to.nil?
       message.cc = normalize_recipients(message.cc).flatten.uniq unless message.cc.nil?
       message.bcc = normalize_recipients(message.bcc).flatten.uniq unless message.bcc.nil?
+      
+      mail.perform_deliveries = false if message.to.empty? && message.cc.empty? && message.bcc.empty?
     end
 
     private
@@ -25,22 +24,12 @@ module MailInterceptor
     def normalize_recipients recipients
       return Array.wrap(recipients) unless env.intercept?
 
-      return forward_emails_to if deliver_emails_to.empty?
-
       recipients.map do |recipient|
         if deliver_emails_to.find { |regex| Regexp.new(regex, Regexp::IGNORECASE).match(recipient) }
           recipient
         else
-          forward_emails_to
+          nil
         end
-      end
-    end
-
-    def sanitize_forward_emails_to
-      self.forward_emails_to = Array.wrap forward_emails_to
-
-      if forward_emails_to_empty? && env.intercept?
-        raise "forward_emails_to should not be empty"
       end
     end
 
